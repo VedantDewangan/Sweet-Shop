9a02b0a693fbd99cdbaaaac6bdb92e5e79c87ec7
VedantDewangan <135214899+VedantDewangan@users.noreply.github.com>
2025-09-26 12:54:09 +0530

    Create AdminHome.jsx

---
+++ frontend/src/Admin/AdminHome/AdminHome.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import "./AdminHome.css";
+import axios from "axios";
+import AdminFoodCard from "../../components/AdminCard/AdminFoodCard";
+
+export default function AdminHome() {
+  const [menu, setMenu] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getAllFood = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:3000/api/sweets", {
+          withCredentials: true,
+        });
+        setMenu(data.all_sweet);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getAllFood();
+  }, []);
+
+  return (
+    <div className="page admin-page">
+      <Navbar />
+
+      <div className="admin-controls">
+        <button className="back-btn" onClick={() => navigate(-1)}>
+          ← Go Back
+        </button>
+        <button className="add-btn" onClick={() => navigate("/admin/add-page")}>
+          ＋ Add New
+        </button>
+      </div>
+
+      <div className="all-food-con">
+        {menu.map((obj) => (
+          <AdminFoodCard
+            key={obj._id || obj.name}
+            setMenu={setMenu}
+            food={obj}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
