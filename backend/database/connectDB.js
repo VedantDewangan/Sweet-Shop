22b39c0db779f8349576c0372d04ca2623800a52
VedantDewangan <135214899+VedantDewangan@users.noreply.github.com>
2025-09-26 12:34:23 +0530

    Create connectDB.js

---
+++ backend/database/connectDB.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Connot connect with Database");
+  }
+};
+
+module.exports = { connectDB };
