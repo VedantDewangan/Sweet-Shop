787a5f602f10d49231c2b631ae021b8cbdfd1f6c
VedantDewangan <135214899+VedantDewangan@users.noreply.github.com>
2025-09-26 13:13:26 +0530

    Update AdminHome.jsx

--- frontend/src/Admin/AdminHome/AdminHome.jsx
+++ frontend/src/Admin/AdminHome/AdminHome.jsx
@@ -4,10 +4,18 @@ import Navbar from "../../components/Navbar/Navbar";
 import "./AdminHome.css";
 import axios from "axios";
 import AdminFoodCard from "../../components/AdminCard/AdminFoodCard";
+import { useAuth } from "../../Auth/AuthContext";
 
 export default function AdminHome() {
   const [menu, setMenu] = useState([]);
   const navigate = useNavigate();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (user.email !== "candy@admin.com") {
+      navigate("/");
+    }
+  }, [user, navigate]);
 
   useEffect(() => {
     const getAllFood = async () => {
